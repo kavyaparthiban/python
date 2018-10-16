2ca3922e08850cb0b45394acd9db2be899f68098
kavyaparthiban <mi0305@en-kavyap.mynt.myntra.com>
2018-10-16 17:12:29 +0530

    check

--- WeightedAlgorithms/setup.py
+++ WeightedAlgorithms/setup.py
@@ -4,8 +4,8 @@ setup(
   packages = ['WeightedAlgorithms'], # this must be the same as the name above
   version = '1',
   description = 'Weighted version of unsupervised techniques',
-  author = 'Kavya Parthiban','Sudarson',
-  author_email = 'kavya@live.co.uk','sudarsontm@outlook.com',
+  author = 'Kavya Parthiban',
+  author_email = 'kavya@live.co.uk',
   url = 'https://github.com/kavyaparthiban/python',
   download_url = 'https://github.com/kavyaparthiban/python/releases/tag/v1.0',
   keywords = ['kmeans', 'weighted'],
